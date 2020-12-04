36f11d7a07a4cd434fd29273b3a37243a9732926
phil <phil.roth077@gmail.com>
2020-12-04 11:13:55 -0500

    04 - Open Office Hours: Advanced Class Methods (fixed)

--- mod-1/04-OOH/class-song.rb
+++ mod-1/04-OOH/class-song.rb
@@ -35,21 +35,21 @@ class Song
   end
 
   def self.genre_count
-    # genre_hash = Hash.new
-    # @@genres.each do | gen |
-    #   genre_hash[gen] = @@genres.count(gen)
-    # end
-    # genre_hash
-
-    gen_count = {}
-    @@genres.map do |gen|
-      if gen_count.has_key?(gen)
-        gen_count += 1
-      else
-        gen_count[gen]
-      end
+    genre_hash = Hash.new
+    @@genres.each do | gen |
+      genre_hash[gen] = @@genres.count(gen)
     end
-    gen_count
+    genre_hash
+
+    # gen_count = {}
+    # @@genres.map do |gen|
+    #   if gen_count.has_key?(gen)
+    #     gen_count += 1
+    #   else
+    #     gen_count[gen]
+    #   end
+    # end
+    # gen_count
   end
 
   # @@genre_hash = Hash.new
