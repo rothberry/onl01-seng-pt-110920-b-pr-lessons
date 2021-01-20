f46e33bb404e80fad6e43e93be98c6c82a6d60ca
phil <phil.roth077@gmail.com>
2021-01-20 13:19:38 -0500

    orm lecture

--- mod-2/03-ORMs/ORMs.md
+++ mod-2/03-ORMs/ORMs.md
@@ -2,6 +2,32 @@
 
 ## What does ORM stand for?
 
+Object Relational Mapping
+
 ## Why do we use ORMs?
 
-## Herdoc & SQLite3 & Ruby
+Matches the SQL Databases into programs.
+It keeps and the code consistent
+
+## SQLite3
+
+a gem that helps us communicate with our sql database
+
+DB = {
+  conn: SQLite3::Database.new('db/movies.db')
+}
+
+## Heredoc
+
+A heredoc is a way to define a multiline string, while maintaining the original indentation & formatting.
+
+Converting ruby methods to ORM
+
+sql query string
+DB[:conn].execute
+match our models to new database insert
+
+INSERT INTO movies (title, genre, year)
+VALUES (?, ?, ?);
+?'s are treated as "bind variable"
+Order does matter for binding variables
--- mod-2/03-ORMs/lib/movie.rb
+++ mod-2/03-ORMs/lib/movie.rb
@@ -1,41 +1,99 @@
 class Movie
 
-  attr_accessor :title, :genre, :actors
+  attr_accessor :title, :genre, :year
   attr_reader   :id
 
   @@all = []
 
-  def initialize(args)
-    args.each do |key, val|
+  # Mass Assignment
+  def initialize(input_hash)
+    # @id = input_hash["id"]
+    # @title = input_hash["title"]
+    # @genre = input_hash["genre"]
+    # @year = input_hash["year@year"]
+
+    input_hash.each do |key, val|
+      # @key = val
       self.send("#{key}=", val)
     end
-    @id = @@all.count + 1
+    # @id = input_hash["id"]
+    # @@all << self
+    # @id = @@all.count + 1
   end
 
+  def self.create_table
+    query = <<-DOC
+      CREATE TABLE IF NOT EXISTS movies (
+        id      INTEGER PRIMARY KEY,
+        title   TEXT,
+        genre   TEXT,
+        year    INTEGER
+      );
+    DOC
+    DB[:conn].execute(query)
+  end
+
+  def self.drop_table
+    query = <<-DOC
+      DROP TABLE IF EXISTS movies;
+    DOC
+    DB[:conn].execute(query)
+  end
+  
   def self.all
     @@all
   end
 
-  def save
-    @@all.push(self)
+  def self.all_from_db
+    # @@all
+    query = <<-DOC
+      SELECT * FROM movies;
+    DOC
+    data = DB[:conn].execute(query)
+    # create new movie instances
+    # data.map do |movie_hash|
+    #   Movie.new(movie_hash)
+    # end
+  end
+
+  def add_movie
+    # @@all.push(self)
+    query = <<-DOC
+      INSERT INTO movies (title, genre, year)
+      VALUES (?, ?, ?);
+    DOC
+    data = DB[:conn].execute(query, self.title, self.genre, self.year)
+    # binding.pry
   end
 
   def self.find_by_title(title)
-    self.all.select do |movie|
-      movie.title == title
-    end
+    # self.all.select do |movie|
+    #   movie.title == title
+    # end
+    query = <<-DOC
+      SELECT * FROM movies WHERE title = ?;
+    DOC
+    data = DB[:conn].execute(query, title)
   end
 
   def self.find_by_id(id)
-    self.all.find do |movie|
-      movie.id == id
-    end
+    # self.all.find do |movie|
+    #   movie.id == id
+    # end
+    query = <<-DOC
+      SELECT * FROM movies WHERE id = ?;
+    DOC
+    data = DB[:conn].execute(query, id)
   end
-
+  
   def self.delete_by_id(id)
-    self.all.delete_if do |movie|
-      movie.id == id
-    end
+    # self.all.delete_if do |movie|
+    #   movie.id == id
+    # end
+    query = <<-SQL
+    DELETE FROM movies WHERE id = ?;
+    SQL
+    data = DB[:conn].execute(query, id)
   end
 
 end
